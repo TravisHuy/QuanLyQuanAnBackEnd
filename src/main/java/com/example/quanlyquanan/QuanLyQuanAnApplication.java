a1286f6c6e2fbe7e8a19b24379ec07982f15eaa3
TravisHuy <honhathuy098@gmail.com>
2024-07-07 17:12:31 +0700

    first commit

---
+++ src/main/java/com/example/quanlyquanan/QuanLyQuanAnApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlyquanan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyQuanAnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyQuanAnApplication.class, args);
+    }
+
+}
